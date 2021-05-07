9870747a086f80fd4770e82f45af10b0e9d2bf3c
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2021-05-06 22:00:22 -0400

    Add new Enums representing the static data on MangaDex.

--- asyncdex/__init__.py
+++ asyncdex/__init__.py
@@ -2,3 +2,4 @@ from .client import MangadexClient
 from .exceptions import AsyncDexException, HTTPException, Ratelimit, Unauthorized
 from .ratelimit import Ratelimits
 from .version import version
+from .enum import ContentRating, Demographic, Relationship, MangaStatus, FollowStatus, Visibility
